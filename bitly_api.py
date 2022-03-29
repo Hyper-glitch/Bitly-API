8eae2b7b7262bc7e6b8b01e315eb846b7bac7641
Roman <graff049@gmail.com>
2022-03-29 22:57:48 +0300

    1. create get_clicks_count that return clicks_count 2. add validate_url staticmethod 3. add parse_bitlink staticmethod

--- bitly_api.py
+++ bitly_api.py
@@ -5,6 +5,9 @@ import urllib.parse as urllib
 import requests
 
 
+GENERIC_ACCESS_TOKEN = os.environ.get('GENERIC_ACCESS_TOKEN')
+
+
 class BitlyApi:
     def __init__(self, token):
         self.base_url = 'https://api-ssl.bitly.com/v4/'
@@ -27,11 +30,35 @@ class BitlyApi:
         response.raise_for_status()
         return response.json().get('link')
 
+    def get_clicks_count(self, bitlink):
+        bitlink_without_scheme = BitlyApi.parse_bitlink(bitlink)
+
+        endpoint = f'bitlinks/{bitlink_without_scheme}/clicks/summary'
+        url = urllib.urljoin(self.base_url, endpoint)
+        response = self.session.get(url=url)
+        response.raise_for_status()
+        clicks_count = response.json().get('total_clicks')
+        return clicks_count
+
+    @staticmethod
+    def validate_url(long_url):
+        response = requests.get(url=long_url)
+        response.raise_for_status()
+
+    @staticmethod
+    def parse_bitlink(bitlink):
+        parsed_bitlink = urllib.urlparse(bitlink)
+        bitlink_without_scheme = parsed_bitlink.netloc + parsed_bitlink.path
+        return bitlink_without_scheme
+
 
 if __name__ == '__main__':
-    GENERIC_ACCESS_TOKEN = os.environ.get('GENERIC_ACCESS_TOKEN')
+    long_url = str(input())
 
     bitly_instance = BitlyApi(token=GENERIC_ACCESS_TOKEN)
     user = bitly_instance.get_user()
-    bitlink = bitly_instance.create_bitlink(long_url='https://music.youtube.com')
-    print(user, bitlink)
+    BitlyApi.validate_url(long_url=long_url)
+    bitlink = bitly_instance.create_bitlink(long_url=long_url)
+    clicks_count = bitly_instance.get_clicks_count(bitlink=bitlink)
+
+    print(f'Битлинк {bitlink}\n', f'Количество кликов: {clicks_count}')
