912fe85ac1ed54fec8c94f3ebb22a7f90438bb10
Roman <graff049@gmail.com>
2022-04-02 21:59:58 +0300

    1. add docstrings

--- bitly_api.py
+++ bitly_api.py
@@ -11,13 +11,19 @@ BASE_API_URL = 'https://api-ssl.bitly.com/v4/'
 
 
 class BitlyApi:
+    """Class for creating Bitly API instance."""
     def __init__(self, token):
+        """Initiate Bitly API instance.
+        Args:
+            token: personal access token for authorization to Bitly API.
+        """
         self.base_url = BASE_API_URL
         self.OAuth_2 = {'Authorization': f'Bearer {token}'}
         self.session = requests.Session()
         self.session.headers.update(self.OAuth_2)
 
     def get_user(self) -> dict:
+        """Send request to API's 'user' endpoint and return user info in dict data structure."""
         endpoint = 'user'
         url = urllib.urljoin(self.base_url, endpoint)
         response = self.session.get(url=url)
@@ -25,6 +31,7 @@ class BitlyApi:
         return response.json()
 
     def create_bitlink(self, long_url) -> str:
+        """Send request to API's 'bitlinks' endpoint and return bitlink."""
         endpoint = 'bitlinks'
         body = json.dumps({'long_url': long_url})
         url = urllib.urljoin(self.base_url, endpoint)
@@ -33,6 +40,7 @@ class BitlyApi:
         return response.json().get('link')
 
     def get_total_clicks(self, bitlink) -> int:
+        """Send request to API's to get total clicks on bitlink return total clicks count."""
         bitlink_without_scheme = parse_bitlink(bitlink)
 
         endpoint = f'bitlinks/{bitlink_without_scheme}/clicks/summary'
@@ -44,17 +52,20 @@ class BitlyApi:
 
 
 def validate_response(long_url):
+    """Send get request and check response status, if it's OK , url validate, else raise exception."""
     response = requests.get(url=long_url)
     response.raise_for_status()
 
 
 def parse_bitlink(bitlink) -> str:
+    """Parse and return bitlink without a scheme."""
     parsed_bitlink = urllib.urlparse(bitlink)
     bitlink_without_scheme = parsed_bitlink.netloc + parsed_bitlink.path
     return bitlink_without_scheme
 
 
 def is_url_bitlink(long_url) -> bool:
+    """Check if the url is a bitlink."""
     parsed_url = urllib.urlparse(long_url)
     if parsed_url.hostname == BITLY_HOSTNAME:
         return True
@@ -62,6 +73,10 @@ def is_url_bitlink(long_url) -> bool:
 
 
 def main(long_url):
+    """Start the main logic of the program."""
+    bitly_instance = BitlyApi(token=GENERIC_ACCESS_TOKEN)
+    bitly_instance.get_user()
+
     is_bitlink = is_url_bitlink(long_url)
     if is_bitlink:
         bitlink = long_url
@@ -73,9 +88,6 @@ def main(long_url):
 
 
 if __name__ == '__main__':
-    bitly_instance = BitlyApi(token=GENERIC_ACCESS_TOKEN)
-    user = bitly_instance.get_user()
-
     long_url = str(input('Введите ссылку: '))
     validate_response(long_url)
     main(long_url)
